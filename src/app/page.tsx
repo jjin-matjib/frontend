a1a91d68355b17b65c1baed23d632b0f6d9ef822
scato3 <tlsgustn93@naver.com>
2026-07-05 17:56:48 +0900

    chore: 메인 페이지 타이틀 변수 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
+  const title = "찐맛집";
   return (
     <main className="flex flex-1 items-center justify-center">
-      <h1 className="text-2xl font-semibold">찐맛집</h1>
+      <h1 className="text-2xl font-semibold">{titel}</h1>
     </main>
   );
 }
